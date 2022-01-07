2bcfc630aa9fc4bfd3190ee70c5db3622aa983d2
Haeseong Jeon <seong0428@gmail.com>
2022-01-07 23:08:59 +0900

    Initial commit

---
+++ src/main/kotlin/kr/mashup/branding/recruitpoc/RecruitPocApplication.kt
@@ -0,0 +1,11 @@
+package kr.mashup.branding.recruitpoc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RecruitPocApplication
+
+fun main(args: Array<String>) {
+	runApplication<RecruitPocApplication>(*args)
+}
---
+++ src/test/kotlin/kr/mashup/branding/recruitpoc/RecruitPocApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.mashup.branding.recruitpoc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RecruitPocApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
