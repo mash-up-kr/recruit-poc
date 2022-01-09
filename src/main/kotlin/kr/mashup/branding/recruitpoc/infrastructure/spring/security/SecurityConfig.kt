1b2218463c34caf2ad81263c04e80feb40099f86
Haeseong Jeon <seong0428@gmail.com>
2022-01-09 12:22:15 +0900

    config: h2-console 사용

--- src/main/kotlin/kr/mashup/branding/recruitpoc/infrastructure/spring/security/SecurityConfig.kt
+++ src/main/kotlin/kr/mashup/branding/recruitpoc/infrastructure/spring/security/SecurityConfig.kt
@@ -2,18 +2,25 @@ package kr.mashup.branding.recruitpoc.infrastructure.spring.security
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 
-@EnableWebSecurity
+@EnableWebSecurity(debug = true)
 @Configuration
 class SecurityConfig : WebSecurityConfigurerAdapter() {
+    override fun configure(web: WebSecurity) {
+        web.ignoring()
+            .antMatchers("/h2-console/**")
+    }
+
     override fun configure(http: HttpSecurity) {
         http.authorizeRequests()
             .antMatchers("/").permitAll()
             .antMatchers("/web").permitAll()
             .antMatchers("/web/login").permitAll()
             .antMatchers("/web/teams").permitAll()
+            .antMatchers("/h2-console/**").permitAll()
             .anyRequest().hasAuthority("ROLE_USER")
         http.oauth2Login()
     }
